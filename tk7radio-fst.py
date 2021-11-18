8cfaa4a0e88ddecdde0f56d993bf75c6e10fa3ca
Syed Murad <89536015+syedmurad1@users.noreply.github.com>
2021-11-18 10:11:54 +0000

    Add files via upload

---
+++ tk7radio-fst.py
@@ -0,0 +1,25 @@
+from tkinter import *
+#from PIL import ImageTk,Image
+
+root = Tk()
+root.title('syedmurad.com')
+
+
+r = IntVar()
+# r.set("2")
+
+
+
+def clicked(value):
+	myLabel = Label(root, text=value)
+	myLabel.pack()	
+
+Radiobutton(root, text="Option 1", variable=r, value=1, command=lambda: clicked(r.get())).pack()
+Radiobutton(root, text="Option 2", variable=r, value=2, command=lambda: clicked(r.get())).pack()
+
+
+myButton = Button(root, text="Click Me!", command=lambda: clicked(r.get()))
+myButton.pack()
+
+
+mainloop()
