121f99ffbc5430d98291e1e2b0df7cc521758267
Syed Murad <89536015+syedmurad1@users.noreply.github.com>
2021-11-25 11:35:50 +0000

    Update tk5images.py

--- tk5images.py
+++ tk5images.py
@@ -1,24 +1,14 @@
 from tkinter import *
 from PIL import ImageTk,Image
 
-
 root = Tk()
 root.title('syedmurad.com')
-
-
 
 my_img = ImageTk.PhotoImage(Image.open("murad.jpg"))
 my_label = Label(image=my_img)
 my_label.pack()
 
-
-
-
 button_quit = Button(root, text="Exit Program", command=root.quit)
 button_quit.pack()
-
-
-
-
 
 root.mainloop()
